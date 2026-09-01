790d7b17640150554360d9a4d0c9e39a0fb44bf3
MaeNoSurname <62689776+MaeNoSurname@users.noreply.github.com>
2026-09-02 00:40:27 +1000

    Add force gun visual feedback

--- Assets/ForceGun.cs
+++ Assets/ForceGun.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class ForceGun : MonoBehaviour
 {
     public List<Rigidbody> hitTargets = new List<Rigidbody>();
